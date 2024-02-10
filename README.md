978964bd5ee11458cbcfc87be75ed25ccdcf2b22
Jaeyoung Jo <wodud3743@gmail.com>
2024-02-10 19:16:49 +0900

    Fix code implementation bug

--- README.md
+++ README.md
@@ -8,5 +8,4 @@ https://www.youtube.com/watch?v=XeWG5D71gC0&t=2097s
 https://www.ipb.uni-bonn.de/html/teaching/photo12-2021/2021-pho2-16-ekf-slam.pptx.pdf
 
 ## Example
-<img width="80%" src="https://github.com/jaeyoungjo99/EKF_SLAM_MATLAB/assets/96559530/1edaa69f-5163-4a0e-8395-4136b6a47223"/>
-
+<img width="100%" src="https://github.com/jaeyoungjo99/EKF_SLAM_MATLAB/assets/96559530/14ffdc08-908f-4b2e-b6fc-111c4e08c01d"/>
--- ekf_slam2.m
+++ ekf_slam2.m
@@ -5,31 +5,45 @@ clf
 mapSize = 10; % 맵의 크기 (10m x 10m)
 numLandmarks = 10; % 랜드마크의 수
 landmarks = mapSize * rand(2, numLandmarks); % 랜드마크의 위치를 무작위로 초기화
+
+landmarks(1:2,1) = [0;0];
+landmarks(1:2,2) = [5;0];
+landmarks(1:2,3) = [10;0];
+landmarks(1:2,4) = [10;5];
+landmarks(1:2,5) = [10;10];
+landmarks(1:2,6) = [5;10];
+landmarks(1:2,7) = [0;10];
+landmarks(1:2,8) = [0;5];
+landmarks(1:2,9) = [7;3];
+landmarks(1:2,10) = [3;3];
+
 robotPose = [5; 1; 0]; % 로봇의 초기 위치 (맵 중앙) 및 방향 (0 라디안)
 
 
 % 모션 모델 파라미터
-velocity = 0.5; % 로봇의 속도 (m/s)
+velocity = 0.7; % 로봇의 속도 (m/s)
 yawRate = 0.15; % 로봇의 요율 (rad/s)
 
 % 시뮬레이션 파라미터
 detection_range = 4.0;
-simulationTime = 60; % 시뮬레이션 시간 (초)
+simulationTime = 80; % 시뮬레이션 시간 (초)
 dt = 1; % 시간 간격 (초)
 sim_pause = 0.0;
 
 % EKF SLAM 초기화
 slamState = [robotPose; zeros(numLandmarks*2, 1)]; % 로봇 상태 및 랜드마크 위치
-slamCovariance = blkdiag(zeros(3,3), 100 * eye(2*numLandmarks));
-motionNoise = 0.0015 * eye(3 + 2*numLandmarks); % 모션 노이즈
-observationNoise = [0.2, 0;
-                    0, 7*pi/180.0]; % 관측 노이즈
-sim_observationNoise = [0.1, 0;
-                        0, 5*pi/180.0];
+landmarkInit = zeros(1,numLandmarks);
+landmarkKnown = zeros(1,numLandmarks);
+slamCovariance = blkdiag(zeros(3,3), 10000 * eye(2*numLandmarks));
+motionNoise = 0.0005 * eye(3); % 모션 노이즈 x y yaw
+sim_motionNoise = [0.1, 0.1]; % 모션 노이즈 Vel, Yawrate
+observationNoise = [0.3, 0;
+                    0, 10*pi/180.0]; % 관측 노이즈
+sim_observationNoise = [0.1 3*pi/180.0];
 
-% 시각화 초기화
-
-% plotSlamCovariance(slamCovariance);
+% 이미 위치 아는 랜드마크
+% idx = 2;
+% slamCovariance(3 + idx*2-1:3 + idx*2, 3 + idx*2-1:3 + idx*2) = eye(2)*0.0;
 
 figure;
 
@@ -40,12 +54,19 @@ figure;
 for t = 0:dt:simulationTime
     cla; % 현재 축의 내용을 지웁니다.
     clf;
+    
+    % 진행상황 시각화
+    subplot(1,2,1);
+
+    hold on;
+    grid on;
+
     % 모션 업데이트 sim
     robotPose = motionUpdate(robotPose, velocity, yawRate, dt);
 
     % EKF 예측 단계
     [slamState, slamCovariance] = ekfPrediction(slamState, slamCovariance, ...
-        velocity, yawRate, motionNoise, dt);
+        velocity+sim_motionNoise(1)*(rand-0.5), yawRate+sim_motionNoise(2)*(rand-0.5), motionNoise, dt);
     % 관측 및 EKF 갱신
     for i = 1:numLandmarks
 
@@ -53,27 +74,28 @@ for t = 0:dt:simulationTime
             continue
         end
 
+        line([robotPose(1) landmarks(1,i)], [robotPose(2) landmarks(2,i)], ...
+            'Color','red','LineStyle','--');
+
         z = observeLandmark(robotPose, landmarks(:,i), sim_observationNoise); % sim
         [slamState, slamCovariance] = ekfUpdate(slamState, slamCovariance, ...
-            z, i, observationNoise);
-    end
+            z, i, observationNoise,landmarkInit);
 
+        landmarkInit(i) = 1;
+    end
+    
     landmarkPoses = reshape(slamState(4:(3+numLandmarks*2)), 2, []);
 
-    % 진행상황 시각화
-    subplot(1,2,1);
 
-    hold on;
-    grid on;
 
-    plot(robotPose(1), robotPose(2), 'bo', 'MarkerSize', 4);
-    plot(slamState(1), slamState(2), 'go', 'MarkerSize', 4);
-    plot(landmarks(1,:), landmarks(2,:), 'k+', 'MarkerSize', 5, 'LineWidth', 1);
-    plot(landmarkPoses(1, :), landmarkPoses(2, :), 'rx', 'MarkerSize', 6);
-    error_ellipse(slamCovariance(1:2, 1:2), robotPose(1:2), 'b', 'conf', 0.95);
+    plot(robotPose(1), robotPose(2), 'go', 'MarkerSize', 4); % True pose
+    plot(slamState(1), slamState(2), 'bo', 'MarkerSize', 4); % Estimated pose
+    plot(landmarks(1,:), landmarks(2,:), 'k+', 'MarkerSize', 5, 'LineWidth', 1); % True Landmarks
+    plot(landmarkPoses(1, :), landmarkPoses(2, :), 'rx', 'MarkerSize', 6); % Estimated Landmarks
+    error_ellipse(slamCovariance(1:2, 1:2), slamState(1:2), 'b', 'conf', 0.95); % Estimated cov
 
     for i = 1:numLandmarks
-        if slamState(3 + i*2 -1) ~= 0.0 && slamState(3 + i*2 )
+        if landmarkInit(i) == 1
             error_ellipse(slamCovariance(3 + i*2-1:3 + i*2, 3 + i*2-1:3 + i*2), ...
                             slamState(3 + i*2 -1:3 + i*2), 'r', 'conf', 0.95);
         end
@@ -105,17 +127,20 @@ function newPose = motionUpdate(pose, v, omega, dt) % for sim
     end
 end
 
-function newPose = StateMotionUpdate(pose, v, omega, dt)
+function newPose = StateMotionUpdate(state, v, omega, dt)
     % 로봇의 새로운 포즈 계산
-    FT = [eye(3); zeros(20,3)]; % 23*3
-    newPose = pose + FT *  [(-v/omega)*sin(pose(3)) + (v/omega)*sin(pose(3) + omega*dt);
-                       (v/omega)*cos(pose(3)) - (v/omega)*cos(pose(3) + omega*dt);
+    state_length = length(state);
+    FT = [eye(3); zeros(state_length-3,3)]; % 23*3
+    newPose = state + FT *  [(-v/omega)*sin(state(3)) + (v/omega)*sin(state(3) + omega*dt);
+                       (v/omega)*cos(state(3)) - (v/omega)*cos(state(3) + omega*dt);
                        omega*dt];
 
 end
 
 % EKF 예측 단계 함수
 function [newState, newCovariance] = ekfPrediction(state, covariance, v, omega, noise, dt)
+    landmark_num = (length(state) - 3)/2;
+
     % 상태 벡터의 모션 모델 부분을 업데이트
     newState = StateMotionUpdate(state, v, omega, dt);
     
@@ -132,10 +157,12 @@ function [newState, newCovariance] = ekfPrediction(state, covariance, v, omega, 
     end
     
     % 공분산 업데이트
-    I_diag = eye(2*10); % 20x20 단위 행렬
+    I_diag = eye(2*landmark_num); % 20x20 단위 행렬
     GI = blkdiag(G, I_diag);
     GTI = blkdiag(G', I_diag);
-    newCovariance = GI * covariance * GTI + noise;
+    F = zeros(3,2*landmark_num+3);
+    F(1:3, 1:3) = eye(3);
+    newCovariance = GI * covariance * GTI + F' * noise * F;
 end
 
 % 랜드마크 관측 함수
@@ -147,36 +174,39 @@ function z = observeLandmark(pose, landmark, noise)
     phi = atan2(dy, dx) - pose(3);
     
     % 관측 벡터
-    z = [r + noise(1); phi + noise(2)];
+    z = [r + noise(1)*(rand-0.5); phi + noise(2)*(rand-0.5)];
 end
 
 % EKF 갱신 단계 함수
-function [newState, newCovariance] = ekfUpdate(state, covariance, z, landmarkIndex, noise)
+function [newState, newCovariance] = ekfUpdate(state, covariance, z, landmarkIndex, noise,landmarkInit)
+    landmark_num = (length(state) - 3)/2;
+
     % 랜드마크의 위치 추정
 
-    % new landmark!
-    if(state(3+2*landmarkIndex-1) == 0 ...
-        && state(3+2*landmarkIndex) == 0)
+    if landmarkInit(landmarkIndex) == 0 % new landmark!
         
         state(3+2*landmarkIndex-1) = state(1) + z(1)*cos(z(2) + state(3));
         state(3+2*landmarkIndex) = state(2) + z(1)*sin(z(2) + state(3));
-
-        % fprintf("New id %d, x %f, y %f \n",landmarkIndex,uj(1),uj(2));
     end
 
     landmarkPos = state(3+2*landmarkIndex-1:3+2*landmarkIndex);
 
     % 관측 예측
     zPred = observeLandmark(state(1:3), landmarkPos, zeros(2,1));
-
     innovation = z - zPred;
+
+    while innovation(2) > pi
+        innovation(2) = innovation(2) - 2*pi;
+    end
+    while innovation(2) < -pi
+        innovation(2) = innovation(2) + 2*pi;
+    end
 
     del = [landmarkPos(1) - state(1);
            landmarkPos(2) - state(2)];
     q = del'*del;
 
-
-    F = zeros(5,23);
+    F = zeros(5,2*landmark_num+3);
     F(1:3,1:3) = eye(3);
     F(4:5,3+2*(landmarkIndex-1) + 1: 3+2*(landmarkIndex-1) + 2) = eye(2);
 
@@ -189,27 +219,14 @@ function [newState, newCovariance] = ekfUpdate(state, covariance, z, landmarkInd
 
     
     % 상태 및 공분산 업데이트
-    % newState = state
-    
-    % 23*1 = 23*2 2*1
-    % K_inno = K*innovation
-    newState = state + K*innovation;
-    % for i = 1:10
-    %     newState(3+2*i-1:3+2*i)
-    %     K_inno(3+2*i,1:2)'
-    %     newState(3+2*i-1:3+2*i) = newState(3+2*i-1:3+2*i) + K_inno(3+2*i,1:2)';
-    % end
 
-    newCovariance = (eye(23) - K*H)*covariance;
+    % 23*1 = 23*2 2*1
+    newState = state + K*innovation;
+
+    newCovariance = (eye(2*landmark_num+3) - K*H)*covariance;
 end
 
 function error_ellipse(covariance, center, color, varargin)
-    % error_ellipse - 공분산 행렬과 중심점을 기반으로 타원을 그립니다.
-    %
-    % 입력:
-    %   covariance - 2x2 공분산 행렬
-    %   center - 타원의 중심을 나타내는 [x, y] 형태의 벡터
-    %   varargin - 추가 옵션 (예: 'Confidence', 0.95)
 
     p = inputParser;
     addOptional(p, 'Confidence', 0.95, @(x) x > 0 && x < 1);
@@ -260,7 +277,7 @@ function plotSlamCovariance(slamCovariance)
 
     % 컬러바의 최소값과 최대값을 지정
     minValue = 0; % 예시 최소값
-    maxValue = 2; % 예시 최대값
+    maxValue = 1.5; % 예시 최대값
     caxis([minValue maxValue]);
 
     % 컬러바 추가
